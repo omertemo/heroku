210df2b07c5358f833d6315110a0e431df928f28
omertemo <omer_temo@hotmail.com>
2022-02-27 15:59:59 +0300

    Publications and Subscribe

--- client/app/public/pages/postItem/index.js
+++ client/app/public/pages/postItem/index.js
@@ -1,7 +1,7 @@
 Template.postItem.helpers({
   domain: function () {
-    var a = document.createElement("a");
-    a.href = this.url;
-    return a.hostname;
+    var a = document.createElement("a"); //İlk olarak, boş bir archor ( a ) HTML öğesi oluşturuyoruz ve bunu bellekte saklıyoruz.
+    a.href = this.url; //Daha sonra href niteliğini mevcut gönderinin URL'sine eşit olacak şekilde ayarladık (az önce gördüğümüz gibi, bir yardımcıda this, şu anda üzerinde işlem yapılan nesnedir).
+    return a.hostname; //Son olarak, URL'nin geri kalanı olmadan bağlantının alan adını geri almak için bir öğenin özel hostname özelliğinden yararlanırız.
   },
 });
